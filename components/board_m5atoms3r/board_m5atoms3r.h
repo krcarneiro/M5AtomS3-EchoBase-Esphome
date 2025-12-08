b48ad4c9c391445476f265ef43251d2cbfb71e59
krcarneiro <krcarneiro@gmail.com>
2025-12-08 13:10:55 -0300

    Create board_m5atoms3r.h

---
+++ components/board_m5atoms3r/board_m5atoms3r.h
@@ -0,0 +1,21 @@
+// Ref: https://github.com/thegroove/esphome-custom-component-examples.git
+
+#pragma once
+
+#include <M5Unified.h>
+#include "esphome/core/component.h"
+
+
+namespace esphome {
+namespace board_m5atoms3r {
+
+class BoardM5AtomS3R : public Component {
+ public:
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
+};
+
+
+}  // namespace empty_component
+}  // namespace esphome
